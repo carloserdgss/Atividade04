42971f33e75684eaecd39bbb91255d44045f484e
Carlos Rodrigues Católica <carlos.rodrigues@catolicasc.edu.br>
2025-04-04 17:09:28 -0300

    3_Carlos_Rodrigues.c

--- 3_Carlos_Rodrigues.c
+++ 3_Carlos_Rodrigues.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-
 int main() {
   
 float total, macas;
-  printf("Digite quantas macas foram compradas!\n");
+  printf("Informe a quantidade de macas adquiridas!\n");
   scanf(" %f", &macas);
 
   if (macas >= 12) {
     total = macas * 1.95;
-    printf("O total a pagar Ã©: %.2f", total);
-  } else {
+    printf("O total a pagar eh de: %.2f", total);
+  } 
+  else {
     total = macas * 2.30;
-    printf("O total a pagar Ã©: %.2f", total);
+    printf("O total a pagar eh de: %.2f", total);
   }
   return 0;
 }
